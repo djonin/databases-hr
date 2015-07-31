a502645bc8a2e93c78cca6d4e511fd6c918a9396
rahulbhobe <rahulbhobe@yahoo.com>
2015-07-31 15:01:22 -0400

    Some implementation for get/post

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,12 +1,50 @@
+var Promise = require('bluebird');
 var models = require('../models');
-var bluebird = require('bluebird');
+var mysql    = Promise.promisifyAll(require('mysql'));
 
+var pool = mysql.createPool({
+  connectionLimit: 100,
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: chat,
+  debug: false
+});
 
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) {
+      pool.getConnectionAsync()
+      .then(function(connection) {
+        console.log('connected as id:', connection.threadId);
+        return connection.queryAsync('SELECT * FROM messages');
+      })
+      .then(function(rows) {
+        res.json(rows);
+      })
+      .catch(function(e, connection)) {
+        connection.release();
+        res.json({code: 100, status: 'Error in connection database'});
+      }
+    }, // a function which handles a get request for all messages
+
+    post: function (req, res) {
+      pool.getConnectionAsync()
+      .then(function(connection) {
+        var obj = req.body;
+        console.log('obj:', obj);
+        return connection.queryAsync('INSERT INTO messages SET ?', obj);
+      })
+      .then(function(result) {
+        res.send(200, result);
+      })
+      .catch(function(e, connection)) {
+        connection.release();
+        res.json({code: 100, status: 'Error in connection database'});
+      }
+
+    } // a function which handles posting a message to the database
   },
 
   users: {
